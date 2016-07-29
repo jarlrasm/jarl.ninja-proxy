10a03b7024cccdf1aba883dddc2eea31df823845
Jarl Rasmussen <jarl@okb.no>
2016-07-29 13:50:40 +0200

    Try again ffs

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-certbot certonly -n --standalone -d jarl.ninja --agree-tos --email jarl@jarl.ninja --reinstall
+certbot certonly -n --standalone -d jarl.ninja -d www.jarl.ninja --agree-tos --email jarl@jarl.ninja --reinstall
 
 nginx -g "daemon off;"
